4bda3cdf2f114351dc2a175c3900bbba3583a0cc
kavacharya <128692688+kavacharya@users.noreply.github.com>
2023-03-24 11:41:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ streamlit.header('🍌🥭Build your own smoothie 🥝🍇')
 
 my_fruits_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruits_list=my_fruits_list.set_index('Fruit')
-streamlit.multiselect('Pick some fruits', list(my_fruits_list.index),['Avacado','Strawberries'])
+streamlit.multiselect('Pick some fruits', list(my_fruits_list.index),['Avocado','Strawberries'])
 streamlit.dataframe(my_fruits_list)
