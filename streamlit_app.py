323d2d22d34ebbedc73a4c9ba6feec7ed0593e72
kavacharya <128692688+kavacharya@users.noreply.github.com>
2023-03-24 11:03:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,6 @@ streamlit.text('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭Build your own smoothie 🥝🍇')
 
 my_fruits_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruits_list=set_index('Fruit')
 streamlit.multiselect('Pick some fruits', list(my_fruits_list.index))
 streamlit.dataframe(my_fruits_list)
