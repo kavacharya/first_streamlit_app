d7a6403f795d7d3ec69bccf9c01faee618765347
kavacharya <128692688+kavacharya@users.noreply.github.com>
2023-03-24 10:39:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 
-streamlit.title('i Love dog')
-streamlit.header('Dog')
-streamlit.text('Hazel')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
