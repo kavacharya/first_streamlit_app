ee97283dae556e1323a304e526d269b4dd358ea9
kavacharya <128692688+kavacharya@users.noreply.github.com>
2023-03-28 18:21:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,21 +35,28 @@ try:
  else:
   back_from_function=get_fruityvice_data(fruit_choice)
   streamlit.dataframe(back_from_function)
- 
 except URLError as e:
   streamlit.error()
 
 def get_fruit_load_list():
  with my_cnx.cursor() as my_cur:
-  my_cur.execute("Select * from fruit_load_list")
-  return my_cur.fetchall()
+      my_cur.execute("Select * from fruit_load_list")
+      return my_cur.fetchall()
  
 if streamlit.button('Get fruit load list'):
  my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
  my_data_rows = get_fruit_load_list()
  streamlit.dataframe(my_data_rows)
+ 
+def insert_row_snowflake(new_fruit):
+ with my_cnx.cursor as my_cur:
+  my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from streamlit')")
+  return "Thanks for adding" + new_fruit
 
-add_my_fruit=streamlit.text_input("what fruits would you like to add?",'Jackfruit')
-streamlit.write('Thanks for adding',add_my_fruit)
+add_my_fruit=streamlit.text_input("what fruits would you like to add?")
+if streamlit.button('Add a fruit to the list'):
+ my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+ back_from_function=insert_row_snowflake(add_my_fruit)
+ streamlit.dataframe(back_from_function)
 
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from streamlit')")
+
